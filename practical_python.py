2c0ba5597c70b146616244c202e5944f703c51c4
Roshan S <roshansrini26@gmail.com>
2026-09-19 12:08:06 +0530

    practical python 1

---
+++ practical_python.py
@@ -0,0 +1,25 @@
+"""
+Project structure
+    keep data seperate
+    use clear names
+    simple structure
+    test as you go
+"""
+
+"""
+Python paths
+
+#os.getcwd() returns the working directory
+
+Files
+open() - file reading functions
+use forward slashes data/sales_analyser.py
+
+    with open("data/sales.csv", "r") as file:
+        content = file.read()
+
+Modules
+Use import statements
+    import mymodule
+    from folder.utils import helper
+"""
